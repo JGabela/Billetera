15b47d7cc231a7e211ecb327e925f437232c12bc
JGabela <josemiguelgabela101@gmail.com>
2025-10-15 16:07:13 -0500

    Cambios jose luis

---
+++ src/Modelo/Usuario.java
@@ -0,0 +1,46 @@
+package Modelo;
+
+import java.util.Random;
+
+public class Usuario {
+    private String cedula;
+    private int numeroBilletera;
+    private String usuario;
+    private String clave;
+    private String nombre;
+
+    public Usuario(String cedula, String usuario, String clave, String nombre) {
+        this.cedula = cedula;
+        this.usuario = usuario;
+        this.clave = clave;
+        this.nombre = nombre;
+        this.numeroBilletera = generarNumeroBilletera();
+    }
+
+    //genera un numero random para la billetera
+    private int generarNumeroBilletera() {
+        Random random = new Random();
+        return 100000 + random.nextInt(900000);
+    }
+
+    //getters
+    public String getCedula() {
+        return cedula;
+    }
+
+    public int getNumeroBilletera() {
+        return numeroBilletera;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}

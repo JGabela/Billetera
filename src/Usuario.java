5a9e773e10b2003bff5ccd4e5e53d3d16619a14b
JGabela <87086339+JGabela@users.noreply.github.com>
2025-11-24 10:45:00 -0500

    Update Usuario.java
    Avance 24 de noviembre

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,49 +1,44 @@
-import java.util.ArrayList; //Para guardar las transacciones realizadas por el usuario
+package Modelo;
+
+import java.util.Random;
 
 public class Usuario {
-    protected String cedula;
-    protected String nombre;
-    protected String apellido;
-    //private double saldo;
+    private String cedula;
+    private int numeroBilletera;
+    private String usuario;
+    private String clave;
+    private String nombre;
 
-    //Un array list para almacenar los contactos guardados del usuario
-    private ArrayList<Contactos> contactos;
-
-    //Un array list para almacenar las transacciones del usuario
-    private ArrayList<gestionTransacciones> transacciones;
-
-    //Un array list para almacenar las tarjetas
-    private ArrayList<Tarjeta> tarjetas;
-
-
-    public Usuario(String cedula, String nombre, String apellido){
+    public Usuario(String cedula, String usuario, String clave, String nombre) {
         this.cedula = cedula;
+        this.usuario = usuario;
+        this.clave = clave;
         this.nombre = nombre;
-        this.apellido = apellido;
-        //this.saldo = 0.0; //El saldo por defecto es 0
-        this.contactos = new ArrayList<>(); //Se crea el array list de los contactos al usuario
-        this.transacciones = new ArrayList<>(); //Se crea el array list de las transacciones al usuario
+        this.numeroBilletera = generarNumeroBilletera();
     }
 
-    //Añade la transaccion a la lista de estas
-    public void nuevaTransaccion(gestionTransacciones transaccion){
-        transacciones.add(transaccion);
+    private int generarNumeroBilletera() {
+        Random random = new Random();
+        return 100000 + random.nextInt(900000);
     }
 
-    //Crea un nuevo y lo añade a la lista de contactos del usuario
-    public void nuevoContactos(){
-
+    public String getCedula() {
+        return cedula;
     }
-/*
-    //Confirma si el alias a buscar está en la lista de contactos del usuario
-    public Contactos buscarContactos(String alias){
-        for (Contactos contacto : contactos){
-            if (contacto.getAlias().equals(alias)){
-                return contacto; //Si es que el alias esta en la lista de contactos
-            }
-        }
-        return null; //Si es que el alias no es parte de la lista de contactos
-    }
-*/
 
+    public int getNumeroBilletera() {
+        return numeroBilletera;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }

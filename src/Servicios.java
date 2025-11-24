dfc3f708b356ef590ebff3bb2a94ff02bbf52a2b
JGabela <87086339+JGabela@users.noreply.github.com>
2025-11-24 10:42:58 -0500

    Update Servicios.java
    Avance 24 de noviembre

--- src/Servicios.java
+++ src/Servicios.java
@@ -1,4 +1,35 @@
 public class Servicios {
-    protected int id;
-    protected String nombre;
+       private String idServicio;
+    private String nombre;
+    private String empresa;
+
+    public PagarServicio(String idServicio, String nombre, String empresa) {
+        this.idServicio = idServicio;
+        this.nombre = nombre;
+        this.empresa = empresa;
+    }
+
+    public PagarServicio(String nombre, String empresa) {
+        this.nombre = nombre;
+        this.empresa = empresa;
+    }
+
+    public PagarServicio(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdServicio() {
+        return idServicio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    @Override
+
 }
